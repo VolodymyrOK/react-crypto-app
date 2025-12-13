b70e9673ad2a3d82a8e4dc64512538e64eea1007
Volodymyr <vladikon70@gmail.com>
2025-12-13 23:04:49 +0100

    Update AppContent.jsx

--- frontend/src/components/layout/AppContent.jsx
+++ frontend/src/components/layout/AppContent.jsx
@@ -21,7 +21,7 @@ export default function AppContent() {
 
   return (
     <Layout.Content style={contentStyle}>
-      <Typography.Title level={3} style={{ textAlign: 'left', color: '#ccc' }}>
+      <Typography.Title level={3} style={{ textAlign: 'left', color: '#fff' }}>
         Portfolio:{' '}
         {assets
           .map((asset) => asset.amount * cryptoPriceMap[asset.id])
